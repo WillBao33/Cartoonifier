32502acc6022ad8385cc920b43cb19d3f26def80
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2022-11-27 21:05:56 -0500

    Update cartoonifier.py

--- cartoonifier.py
+++ cartoonifier.py
@@ -1,16 +1,15 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-#from scipy import ndimage, signal
-#import skimage
 import argparse
 import time
-#from PIL import Image
+from PIL import Image
 from gaussian_kernel import gaussian_filter
 from bilateral_filter import bilateral_filter
 from non_max_suppression import non_max_suppression
 from double_threshold import threshold
 from edge_tracking import hysteresis
+from edge_2 import edge_mask
 
 
 
@@ -63,17 +62,31 @@ if __name__ == '__main__':
     (thresh, im_bw) = cv2.threshold(edge, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     edge = cv2.bitwise_not(im_bw) #reverse 0 and 255
 
+    #different edge 
+    line_size = 7
+    blur_value = 7
+    edge_2 = edge_mask(resized, line_size, blur_value)
     #bilateral filter on original image
     start = time.time()
     mat = bilateral_filter(resized,radius=5,sigma_color=15,sigma_space=10)
     end = time.time()
     print('processing time = {} min {} s'.format(int((end - start) / 60), int((end - start) % 60)))
-    
+    cv2.imwrite('/home/will/cv_course/bf_2.png',mat)
     #bitwise_and operation
     cartoon=cv2.bitwise_and(mat,mat,mask=edge)
-    cv2.imshow("cartoonized",cartoon)
-    cv2.imwrite(f'{out_image}',cartoon)
+    cartoon_2 = cv2.bitwise_and(mat,mat,mask=edge_2)
+    #cv2.imwrite(f'{out_image}',cartoon)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    plt.figure(figsize=(10, 10))
+    plt.suptitle('Original image')
+    plt.subplot(2, 2, 1), plt.title('image')
+    plt.imshow(cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)), plt.axis('off')
+    plt.subplot(2, 2, 2), plt.title('Cartoonized with Canny')
+    plt.imshow(cv2.cvtColor(cartoon, cv2.COLOR_BGR2RGB)), plt.axis('off')
+    plt.subplot(2, 2, 3), plt.title('Cartoonized with AdaptiveThreshold')
+    plt.imshow(cv2.cvtColor(cartoon_2, cv2.COLOR_BGR2RGB)), plt.axis('off')
+    plt.subplot(2, 2, 4), plt.title('my bilateral filter')
+    plt.imshow(cv2.cvtColor(mat, cv2.COLOR_BGR2RGB)), plt.axis('off')
+    plt.show()
